edb966fd046b35e5212bf3227d401f11f156f9f0
Tiger Yotsawat <tigeryotsawat@gmail.com>
2025-10-05 16:03:22 +0100

    Refactor Blogs section

--- app/(home)/blogs.tsx
+++ app/(home)/blogs.tsx
@@ -1,31 +1,45 @@
 import Link from "next/link";
-import styles from "./blogs.module.css";
 
 export default function Blogs() {
   return (
     <section className="flow grid justify-item-center" id="blogs">
-      <h2 className="text-center section__title section__title--accent">
+      <h2 className="section-title" data-accent>
         Blogs
       </h2>
-      <p className="mx-auto text-center section__subtitle">
-        Things I Think About
-      </p>
+      <p className="section-subtitle">Things I Think About</p>
 
-      <div className={`${styles["blogs"]} mx-auto flow mb-5`}>
-        <div className="blog flow">
-          <Link href="/blogs/#" className="text-neutral-800 fw-bold">
-            Technology trends in 2022
-          </Link>
-          <p className="blog__date">30, May 2022</p>
-          <p className="blog__post"></p>
-        </div>
+      <div className={`mx-auto flow | mb-5 max-w-[600px] lg:max-w-[80%]`}>
+        <article className="flow">
+          <h3 className="ms-0">
+            <Link href="/blogs/#" className="text-neutral-800 font-bold">
+              Technology trends in 2022
+            </Link>
+          </h3>
+          <time dateTime="2022-05-30" className="text-neutral-400 text-sm">
+            May 30, 2022
+          </time>
+          <p className="text-neutral-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+            quos. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor
+            sit amet consectetur adipisicing elit. Quisquam, quos.
+          </p>
+        </article>
 
-        <div className="blog flow">
-          <Link href="/blogs/#" className="text-neutral-800 fw-bold">
-            What I learned from investing
-          </Link>
-          <p className="blog__date">01, Jan 2022</p>
-          <p className="blog__post">
+        <article className="flow">
+          <h3 className="ms-0">
+            <Link href="/blogs/#" className="text-neutral-800 font-bold">
+              What I learned from investing
+            </Link>
+          </h3>
+          <time dateTime="2022-01-01" className="text-neutral-400 text-sm">
+            January 1, 2022
+          </time>
+          <p className="ms-0 text-neutral-400">
             Learning to invest is something that I found really challenging.
             With all the jargons to learn and all the noise and hype to filter
             out from genuine advice, it can be a tough maze to navigate for new
@@ -33,14 +47,18 @@ export default function Blogs() {
             found really useful in learning the basics of investing and to share
             some of the learning points that I came across over the past year.
           </p>
-        </div>
+        </article>
 
-        <div className="blog flow">
-          <Link href="/blogs/#" className="text-neutral-800 fw-bold">
-            My web development journey
-          </Link>
-          <p className="blog__date">30, Dec 2021</p>
-          <p className="blog__post">
+        <article className="flow">
+          <h3 className="ms-0">
+            <Link href="/blogs/#" className="text-neutral-800 font-bold">
+              My web development journey
+            </Link>
+          </h3>
+          <time dateTime="2021-12-30" className="text-neutral-400 text-sm">
+            December 30, 2021
+          </time>
+          <p className="ms-0 text-neutral-400">
             Web development is my favourite hobby. There is just so much to
             learn in this broad field, and with all the different web
             applications that we see and use on a day to day basis, we can all
@@ -53,10 +71,10 @@ export default function Blogs() {
             &lsquo;creators&rsquo; from the &lsquo;users&rsquo;, wondering how
             to scale it.
           </p>
-        </div>
+        </article>
       </div>
 
-      <Link href="/blogs" className="button button--primary mx-auto">
+      <Link href="/blogs" className="button mx-auto" data-variant="primary">
         see more blogs
       </Link>
     </section>
